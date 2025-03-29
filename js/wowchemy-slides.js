75832158c289446d5b4368acb84af1b5089887d5
cangmushui <bty1292008@163.com>
2025-03-29 17:07:51 +0800

    2

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:J:\website\ruim-jlu-site\themes\wowchemy-hugo-themes\wowchemy\assets\js\wowchemy-utils.js
+  // ns-hugo:D:\website\ruim-jlu-site\themes\wowchemy-hugo-themes\wowchemy\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
